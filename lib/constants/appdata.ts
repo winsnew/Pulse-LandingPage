ea6be4b55a99304cb5cc57b85bf5394ca29e022d
Tejinderb <tejinder0732.be20@chitkara.edu.in>
2025-09-22 22:40:36 +0530

    Added Assets

--- lib/constants/appdata.ts
+++ lib/constants/appdata.ts
@@ -24,54 +24,54 @@ export const teamMembers: TeamMembers[] = [
         bio: "Ex Dell Engineer | Author of Dell NativeEdge Cloud | 3X Founder building Tech"
     },
     {
-        id:"4",
-        name: "Harshith Reddy",
-        role: "AI Engineer",
-        image: "/assets/team-members/harshith.jpg",
-        bio: "INEX 2024 | Published ODL+ML=EWS  | Build IDS at International Expo"
-    },
-    {
-        id: "5",
-        name: "Jean Plamin",
-        role: "Full Stack Developer",
-        image: "/assets/team-members/Jean.jpg",
-        bio: "Full-Stack Architect| Enterprise platforms for Fortune 500 | Proven Tech Leadership"
-    },
-    {
-        id: "6",
+        id: "4",
         name: "Tejinder Singh",
         role: "Software Developer",
         image: "/assets/team-members/tejinder.png",
         bio: "Full-Stack Developer | Front-End Engineer | UI-to-Code Specialist | Workflow Automation Developer "
     },
     {
-        id: "7",
+        id: "5",
         name: "SOJO",
         role: "Fullstack Developer",
-        image: "https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg?auto=compress&cs=tinysrgb&w=400",
+        image: "/assets/team-members/Sojo.jpg",
         bio: "Fullstack Web Developer | High Performance | Idea Based Imagined"
     },
     
     {
-        id: "8",
+        id: "6",
         name: "Huy Le",
         role: "Software Developer",
         image: "/assets/team-members/huyle.jpg",
         bio: "Computer Science student who could wear multiple hats"
     },
     {
-        id: "9",
+        id: "7",
         name: "Shubham Saini",
         role: "Marketing Head",
         image: "/assets/team-members/Shub_Saini.jpg",
         bio: "CUDA & Python Dev | 3D Animator | AI/Game Systems | Ex-indusBank Employee | Marketing Dev"
     },
     {
-        id: "10",
+        id: "8",
         name: "Kyle Paul Lexis",
         role: "Community Lead",
         image: "/assets/team-members/Kyle-Paul.jpg",
         bio: "10+ years at Microsoft Azure, expert in scalable AI infrastructure."
+    },
+    {
+        id: "9",
+        name: "Jean Plamin",
+        role: "Full Stack Developer",
+        image: "/assets/team-members/Jean.jpg",
+        bio: "Full-Stack Architect | Enterprise platforms for Fortune 500 | Proven Tech Leadership"
+    },
+     {
+        id:"10",
+        name: "Harshith Reddy",
+        role: "AI Engineer",
+        image: "/assets/team-members/harshith.jpg",
+        bio: "INEX 2024 | Published ODL+ML=EWS  | Build IDS at International Expo"
     },
 ];
 
