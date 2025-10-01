d3e96c9a8d5fb1b6b6bfc02a534b20ea5eeaa18e
Tejinderb <tejinder0732.be20@chitkara.edu.in>
2025-10-02 05:24:14 +0530

    Added Missing Pages

---
+++ app/api/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+import ShinyText from '../../components/customs/shiny-text';
+
+export default function ApiPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center p-8 ">
+
+        <ShinyText
+            baseColor="rgb(255, 255, 255)"
+            shineColor="rgb(255, 255, 255)"
+            speed={2}
+            size="3xl"
+            weight="semibold"
+            direction='left-to-right'
+            pauseOnHover={true}
+        >
+            Coming Soon ... 
+        </ShinyText>
+    </div>
+  );
+}
